0b503641c1c8e29dbdc0a56f7f2845b3709a8916
Alex <alexitamargomes@gmail.com>
2016-06-15 19:47:24 -0300

    15-06-2016

---
+++ backup.bat
@@ -0,0 +1,3 @@
+CD C:\Program Files\TortoiseSVN\bin
+TortoiseProc.exe /command:cleanup /path:"D:\Desenv\Projetos_2009\AnOnImUs\DataBase\" /logmsg:"Backup de Sexta: 03/06/2016" /closeonend:3
+TortoiseProc.exe /command:commit  /path:"D:\Desenv\Projetos_2009\AnOnImUs\DataBase\" /logmsg:"Backup de Sexta: 03/06/2016" /closeonend:3
